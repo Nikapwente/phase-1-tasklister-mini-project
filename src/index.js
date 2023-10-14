b9df1558b6f4bbcc750247f91789c9c193a5cfd4
Nikapwente <hrsmajor@gmail.com>
2023-10-14 18:28:00 +0300

    Assignment Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById('create-task-form');
+  form.addEventListener('submit', appendList)
+
+
+  
 });
+
+
+function appendList(event) {
+  
+  event.preventDefault();
+  let listItem = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x';
+  let list = document.getElementById('tasks');
+
+  listItem.textContent = `${event.target["new-task-description"]["value"]} `;
+  list.append(listItem);
+  listItem.appendChild(btn);
+}
+
+function handleDelete(event) {
+  event.target.parentNode.remove()
+}
